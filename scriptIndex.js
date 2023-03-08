35ab9183b64f4e1899292112856e3747a599ba90
Lisalalalale <62019136+Lisalalalale@users.noreply.github.com>
2023-03-08 20:57:05 +0100

    Update scriptIndex.js

--- scriptIndex.js
+++ scriptIndex.js
@@ -24,5 +24,5 @@ console.log(`textA.html?textSize= ${textSizeInput.value}`);
 
 function changeToText() {
   
-  window.location.href = `textA.html?textSize= ${textSizeInput.value}`;
+  window.location.href = `textB.html?textSize= ${textSizeInput.value}`;
 }
